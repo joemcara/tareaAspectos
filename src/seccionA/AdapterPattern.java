c9bcac086825b987832a0503d209b72649cd4cdd
AndresBorbor <adborbor@espol.edu.ec>
2022-11-07 20:06:44 -0500

    Cambios en Ejemplo Adapter

---
+++ src/seccionA/AdapterPattern.java
@@ -0,0 +1,49 @@
+package seccionA;
+
+public class AdapterPattern {
+	public interface interfazAdapter {
+		public abstract void sacarYenes(double dolares);
+		public abstract void depositarYenes(double dolares);
+	}
+	
+	
+	public class Adaptador implements interfazAdapter{
+		CuentaAhorros ahorros = new CuentaAhorros();
+		public Adaptador(){
+		}
+		public double getSaldo(){
+			return this.ahorros.getSaldoDisponible();
+		}
+
+		@Override
+		public void sacarYenes(double yenes ){
+	 		double dolares = yenes/146.48;
+			ahorros.retirarDinero(dolares);
+		} 
+
+		@Override
+		public void depositarYenes(double yenes){
+			double dolares = yenes/146.48;
+			ahorros.depositar(dolares);
+		}
+	}
+	
+	public class CuentaAhorros {
+		private double dolares;
+		
+		public CuentaAhorros(){
+		} 
+
+		public double getSaldoDisponible(){
+			return this.dolares;
+		} 
+
+		public void retirarDinero(double dolares){
+		 	this.dolares -= dolares;
+		} 
+
+		public void depositar(double dolares){
+			this.dolares += dolares;
+		}
+	}
+}
--- src/seccionA/interfazAdapter.java
+++
@@ -1,6 +0,0 @@
-package seccionA;
-
-public interface interfazAdapter {
-	public abstract void sacarYenes(double dolares);
-	public abstract void depositarYenes(double dolares);
-}
